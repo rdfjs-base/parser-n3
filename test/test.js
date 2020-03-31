c9de768f69887c4202e600afbcd4d90566b9c797
bergi <bergi@axolotlfarm.org>
2020-03-31 16:15:37 +0200

    use readable-to-readable, N3.js upgrade, cleanuped tests

--- test/test.js
+++ test/test.js
@@ -1,106 +1,100 @@
-/* global describe, it */
-
-const assert = require('assert')
+const { strictEqual } = require('assert')
+const getStream = require('get-stream')
+const { describe, it } = require('mocha')
 const sinkTest = require('@rdfjs/sink/test')
 const stringToStream = require('string-to-stream')
 const N3Parser = require('..')
 
-function waitFor (stream) {
-  return new Promise((resolve, reject) => {
-    stream.on('end', resolve)
-    stream.on('error', reject)
-  })
-}
-
 describe('@rdfjs/parser-n3', () => {
   sinkTest(N3Parser, { readable: true })
 
-  it('.import should parse the given string triple stream', () => {
-    const nt = '<http://example.org/subject> <http://example.org/predicate> "object" .'
-    const parser = new N3Parser()
+  describe('.import', () => {
+    it('should parse the given string triple stream', async () => {
+      const nt = '<http://example.org/subject> <http://example.org/predicate> "object" .'
+      const parser = new N3Parser()
 
-    const stream = parser.import(stringToStream(nt))
+      const stream = parser.import(stringToStream(nt))
 
-    return Promise.resolve().then(() => {
-      const quad = stream.read()
+      const results = await getStream.array(stream)
+      const quad = results[0]
 
-      assert.strictEqual(quad.subject.termType, 'NamedNode')
-      assert.strictEqual(quad.subject.value, 'http://example.org/subject')
+      strictEqual(quad.subject.termType, 'NamedNode')
+      strictEqual(quad.subject.value, 'http://example.org/subject')
 
-      assert.strictEqual(quad.predicate.termType, 'NamedNode')
-      assert.strictEqual(quad.predicate.value, 'http://example.org/predicate')
+      strictEqual(quad.predicate.termType, 'NamedNode')
+      strictEqual(quad.predicate.value, 'http://example.org/predicate')
 
-      assert.strictEqual(quad.object.termType, 'Literal')
-      assert.strictEqual(quad.object.value, 'object')
+      strictEqual(quad.object.termType, 'Literal')
+      strictEqual(quad.object.value, 'object')
 
-      assert.strictEqual(quad.graph.termType, 'DefaultGraph')
-
-      return waitFor(stream)
-    })
-  })
-
-  it('.import should parse the given string quad stream', () => {
-    const nt = '<http://example.org/subject> <http://example.org/predicate> "object" <http://example.org/graph> .'
-    const parser = new N3Parser()
-
-    const stream = parser.import(stringToStream(nt))
-
-    return Promise.resolve().then(() => {
-      const quad = stream.read()
-
-      assert.strictEqual(quad.subject.termType, 'NamedNode')
-      assert.strictEqual(quad.subject.value, 'http://example.org/subject')
-
-      assert.strictEqual(quad.predicate.termType, 'NamedNode')
-      assert.strictEqual(quad.predicate.value, 'http://example.org/predicate')
-
-      assert.strictEqual(quad.object.termType, 'Literal')
-      assert.strictEqual(quad.object.value, 'object')
-
-      assert.strictEqual(quad.graph.termType, 'NamedNode')
-      assert.strictEqual(quad.graph.value, 'http://example.org/graph')
-
-      return waitFor(stream)
-    })
-  })
-
-  it('.import should emit prefix events for each prefix', () => {
-    const prefix1 = 'http://example.org/prefix1#'
-    const prefix2 = 'http://example.org/prefix2#'
-
-    const nt = `PREFIX p1: <${prefix1}>
-      PREFIX p2: <${prefix2}>
-      <http://example.org/subject> <http://example.org/predicate> "object" .`
-    const parser = new N3Parser()
-    const prefixes = {}
-
-    const stream = parser.import(stringToStream(nt))
-
-    stream.on('prefix', (prefix, namespace) => {
-      prefixes[prefix] = namespace
+      strictEqual(quad.graph.termType, 'DefaultGraph')
     })
 
-    stream.resume()
+    it('should parse the given string quad stream', async () => {
+      const nt = '<http://example.org/subject> <http://example.org/predicate> "object" <http://example.org/graph> .'
+      const parser = new N3Parser()
 
-    return waitFor(stream).then(() => {
-      assert(prefixes.p1)
-      assert.strictEqual(prefixes.p1.termType, 'NamedNode')
-      assert.strictEqual(prefixes.p1.value, prefix1)
+      const stream = parser.import(stringToStream(nt))
 
-      assert(prefixes.p2)
-      assert.strictEqual(prefixes.p2.termType, 'NamedNode')
-      assert.strictEqual(prefixes.p2.value, prefix2)
+      const results = await getStream.array(stream)
+      const quad = results[0]
+
+      strictEqual(quad.subject.termType, 'NamedNode')
+      strictEqual(quad.subject.value, 'http://example.org/subject')
+
+      strictEqual(quad.predicate.termType, 'NamedNode')
+      strictEqual(quad.predicate.value, 'http://example.org/predicate')
+
+      strictEqual(quad.object.termType, 'Literal')
+      strictEqual(quad.object.value, 'object')
+
+      strictEqual(quad.graph.termType, 'NamedNode')
+      strictEqual(quad.graph.value, 'http://example.org/graph')
     })
-  })
 
-  it('.import should handle parser errors', () => {
-    const nt = '1'
-    const parser = new N3Parser()
+    it('should emit prefix events for each prefix', async () => {
+      const prefix1 = 'http://example.org/prefix1#'
+      const prefix2 = 'http://example.org/prefix2#'
 
-    const stream = parser.import(stringToStream(nt))
+      const nt = `PREFIX p1: <${prefix1}>
+        PREFIX p2: <${prefix2}>
+        <http://example.org/subject> <http://example.org/predicate> "object" .`
+      const parser = new N3Parser()
+      const prefixes = {}
 
-    return new Promise((resolve, reject) => {
-      return waitFor(stream).catch(resolve).then(reject)
+      const stream = parser.import(stringToStream(nt))
+
+      stream.on('prefix', (prefix, namespace) => {
+        prefixes[prefix] = namespace
+      })
+
+      await getStream.array(stream)
+
+      strictEqual(typeof prefixes.p1, 'object')
+      strictEqual(prefixes.p1.termType, 'NamedNode')
+      strictEqual(prefixes.p1.value, prefix1)
+
+      strictEqual(typeof prefixes.p2, 'object')
+      strictEqual(prefixes.p2.termType, 'NamedNode')
+      strictEqual(prefixes.p2.value, prefix2)
+    })
+
+    it('should handle parser errors', async () => {
+      const nt = '1'
+      const parser = new N3Parser()
+
+      const stream = parser.import(stringToStream(nt))
+
+      let error = null
+
+      try {
+        await getStream.array(stream)
+      } catch (err) {
+        error = err
+      }
+
+      strictEqual(typeof error, 'object')
+      strictEqual(error.message.includes('literal'), true)
     })
   })
 })
